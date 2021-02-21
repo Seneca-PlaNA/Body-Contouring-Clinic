ea3d55d3cbca326edf2e5a3c410683d40a03247a
yuanLeeMidori <midori3113133@gmail.com>
2021-02-21 18:15:25 -0500

    Get data in return but can't solve map issue

--- src/VIP/ListAllOffer.js
+++ src/VIP/ListAllOffer.js
@@ -6,37 +6,48 @@ class ListAllOffer extends React.Component {
   constructor() {
     super();
     this.state = {
-      results: [],
+      offers: [],
     };
   }
 
-  componentDidMount () {
-    fetch('http://localhost:3001/offers')
-      .then((res) => {
-        this.setState({ results: res.json() });
+  getOffer() {
+    return new Promise((resolve) => {
+      fetch('http://localhost:3001/offers')
+        .then((response) => response.json())
+        .then((results) => {
+          resolve(results);
+        });
+    });
+  }
+  componentDidMount() {
+    // fetch('http://localhost:3001/offers').then((res) => {
+    //   this.setState({ results: res.json() });
+    // });
+    this.getOffer().then((data) => {
+      this.setState({
+        offers: data,
       });
+    });
   }
 
   render() {
     return (
       <div>
+        {'offer:' + JSON.stringify(this.state.offers[0])}
         <CardColumns>
-          {
-            this.state.results.map( (result) => {
-              <Card>
-                <Card.Img variant="top" src={result.imageURL} />
-                <Card.Body>
-                  <Card.Title>{result.offerName}</Card.Title>
-                  <Card.Text>
-                    {result.description}
-                  </Card.Text>
-                  <Button variant="outline-info" href="/Appointment/Create">
-                    Book
-                  </Button>
-                </Card.Body>
-              </Card>
-            })          
-          }
+          {this.state.offers.map((result) => {
+            console.log('offer: ' + result.offerName);
+            <Card>
+              <Card.Img variant="top" src={result.imageURL} />
+              <Card.Body>
+                <Card.Title>{result.offerName}</Card.Title>
+                <Card.Text>{result.description}</Card.Text>
+                <Button variant="outline-info" href="/Appointment/Create">
+                  Book
+                </Button>
+              </Card.Body>
+            </Card>;
+          })}
         </CardColumns>
       </div>
     );
