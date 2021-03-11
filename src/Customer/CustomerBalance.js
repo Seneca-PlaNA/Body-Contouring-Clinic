a691a113e8182af9c78700768d6ce30601d827df
rko7 <hazel.r.ko@gmail.com>
2021-03-11 11:56:06 -0500

    fixing customer balance in process

--- src/Customer/CustomerBalance.js
+++ src/Customer/CustomerBalance.js
@@ -6,40 +6,41 @@ import { Form, Row, Col, Container, Table } from 'react-bootstrap';
 class CustomerBalance extends React.Component {
   constructor(props) {
     super(props);
-      this.state = {
-        items: [
-          { url: '/Customer/', title: 'Home' },
-          { url: `/Customer/${this.props.id}`, title: 'Profile' },
-          { url: `/Customer/Edit/${this.props.id}`, title: 'Edit Profile' },
-          { url: `/Customer/Balance${this.props.id}`, title: 'Balance' },
-        ],
-        _id: localStorage.getItem('_id'),
-        balanceHistories: [],
-        balance: [],
-        account: [],
-        service: [],
-        accountLevel: [],
-        customerBalance: {},
-      };
+    this.state = {
+     items: [
+      { url: '/Customer/', title: 'Home' },
+      { url: `/Customer/${this.props.id}`, title: 'Profile' },
+      { url: `/Customer/Edit/${this.props.id}`, title: 'Edit Profile' },
+      { url: `/Customer/Balance${this.props.id}`, title: 'Balance' },
+    ], 
+      _id: localStorage.getItem('_id'),
+      balanceHistory: [],
+      balance: [],
+      account: [],
+      service: [],
+    };
   }
 
-  getCustomerBalance(id){
-    return new Promise((resolve) => {
-      fetch(`${process.env.REACT_APP_API_URL}/Balance/${id}`)
+getBalanceHistories(id){
+  return new Promise((resolve) => {
+    fetch(`${process.env.REACT_APP_API_URL}/Balance/${id}`)
       .then((response) => response.json())
       .then((data) => {
         resolve(data);
       });
-    });
-  }
+  });
+}
 
-  componentDidMount(){
-    this.getCustomerBalance(this.state._id).then((data) => {
-      this.setState({
-        customerBalance: data,
-      });
+componentDidMount() {
+  this.getBalanceHistories(this.state.id).then((data) => {
+    this.setState({
+      balanceHistories: data,
+      balance: data.balance,
+      account: data.account,
+      service: data.service
     });
-  }
+  });
+}
 
   render() {
     const pagination = {
@@ -52,21 +53,19 @@ class CustomerBalance extends React.Component {
         <div className="col-md-1"></div>
         <SideBar items={this.state.items} />
         <div className="col-md-6" style={{ 'margin-left': '80px' }}>
-          <h2 className="PageTitle">Hi, {this.state.account.firstName + ' ' + this.state.accountLevel.lastName}</h2>
+          <h2 className="PageTitle">Hi, {this.state.account.firstName + ' ' + this.state.account.lastName}</h2>
           <hr />
           <br />
           <h4>Balance Information</h4>
           <Container class="col-md-6">
-          {this.state.balanceHistories && this.state.balanceHistories.map((bal) => (
-            <Form style={{ fontSize: '20px', marginLeft: '80px', textAlign: 'left' }} key={bal._id}>
-
+            <Form style={{ fontSize: '20px', marginLeft: '80px', textAlign: 'left' }}>
               <Form.Group as={Row}>
                 <Form.Label column md={3}>
                   Current Balance:
                 </Form.Label>
                 <Col sm={2}>
                   <Form.Label column md={0}>
-                    ${bal.balance.balanceAccount}
+                    ${this.state.balance.balanceAccount}
                   </Form.Label>
                 </Col>
                 <Col sm={2}>
@@ -76,21 +75,16 @@ class CustomerBalance extends React.Component {
                 </Col>
                 <Col sm={2}>
                   <Form.Label column md={0}>
-                    {bal.accountLevel.name}
+                    VIP
                   </Form.Label>
                 </Col>
               </Form.Group>
-              
             </Form>
-            ))}
           </Container>
-
           <br />
           <h4>Balance History</h4>
-
           <Container class="col-md-6">
-          { this.state.balanceHistories && this.state.balanceHistories.map((result) => (
-            <Table key={result._id}>
+            <Table>
               <Row>
                 <Col md={12}>
                   <table>
@@ -101,10 +95,10 @@ class CustomerBalance extends React.Component {
                       <th>Price</th>
                     </tr>
                     <tr>
-                      <td>{result.balanceHistories.date}</td>
-                      <td>{result.service.serviceCategory}</td>
-                      <td>{result.service.name}</td>
-                      <td>${result.service.price}</td>
+                      <td>{this.state.balance.balanceAccount}</td>
+                      <td>{this.state.service.serviceCategory}</td>
+                      <td>{this.state.service.name}</td>
+                      <td>{this.state.service.price}</td>
                       <td>
                         <a href="/Customer/BalanceDetail">details</a>
                       </td>
@@ -127,11 +121,9 @@ class CustomerBalance extends React.Component {
                 <a href="#"> &raquo; </a>
               </span>
             </Table>
-            ))}
           </Container>
           <br />
           <br />
-          
         </div>
       </div>
     );
